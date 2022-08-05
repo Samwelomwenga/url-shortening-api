9d6b41c0a0e7714cbe14fe4899fcc6e9ce983438
SAMWEL OMWENGA <samwel.omwenga1@students.jkuat.ac.ke>
2022-08-05 16:51:23 +0300

    Add error function

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-const navBar= document.querySelector('.nav-bar');
-const hamburgerMenu=document.querySelector('.hamber-menu');
+const shortenUrlSubmit = document.querySelector(".shorten-url-submit");
+ const shortenUrl = document.querySelector("#shorten-url")
+const urlShortener = (url) => {
+  fetch(`https://api.shrtco.de/v2/shorten?url=${url} || `)
+    .then((Response) => Response.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data.result.original_link);
+      console.log(data.result.full_short_link);
+    });
+};
+shortenUrlSubmit.addEventListener("click", () => {
+  urlShortener(shortenUrl.value);
+});
 
-hamburgerMenu.addEventListener("click", ()=>{
-    const visibility=navBar.getAttribute("data-visible");
-    if(visibility==="false"){
-        navBar.setAttribute("data-visible",true);
-    }else{
-        navBar.setAttribute("data-visible",false);
-    }
-})
+
+shortenUrl.oninvalid = (e) => {
+  e.target.setCustomValidity("");
+  if (!e.target.validity.valid) {
+    e.target.setCustomValidity("This field cannot be blank");
+  }
+}
+shortenUrl.oninput = (e) => {
+  e.target.setCustomValidity("");
+}
+
+const navBar = document.querySelector(".nav-bar");
+const hamburgerMenu = document.querySelector(".hamber-menu");
+hamburgerMenu.addEventListener("click", () => {
+  const visibility = navBar.getAttribute("data-visible");
+  if (visibility === "false") {
+    navBar.setAttribute("data-visible", true);
+  } else {
+    navBar.setAttribute("data-visible", false);
+  }
+});
