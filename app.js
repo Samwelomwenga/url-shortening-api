ba362a03451ff4699530021295fff77ae88fb5a9
SAMWEL OMWENGA <samwel.omwenga1@students.jkuat.ac.ke>
2023-07-13 23:16:21 +0300

    Debug shorten-url class error feature

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const toLocaleStorage = async () => {
   try {
     let urlObject = await urlShortener(shortenUrl.value);
     let urlObjectJSON = JSON.stringify(urlObject);
-    // console.log(urlObjectJSON)
     let = urlKey = urlObject.code;
     localStorage.setItem(urlKey, urlObjectJSON);
 
@@ -28,14 +27,12 @@ const toLocaleStorage = async () => {
     console.error(error.message);
   }
 };
-// toLocaleStorage();
 const shortenHistoryContainer = document.querySelector(
   ".shorten-history-container"
 );
 const render = async () => {
   const localKey = await toLocaleStorage();
   const localData = JSON.parse(localStorage.getItem(localKey));
-  //  console.log(localData)
   const { code, short_link, original_link } = localData;
   shortenHistoryContainer.insertAdjacentHTML(
     "beforebegin",
@@ -55,70 +52,93 @@ const render = async () => {
 </div>`
   );
 };
-// render();
 
-shortenUrlSubmit.addEventListener("click", () => {
-  // render();
-  // copyDataFunction();
+shortenUrlSubmit.addEventListener("click", async () => {
+  // await copyDataFunction(shortenHistoryContainer);
+  handleDataError();
+  render();
 });
-// const shortenHistory = document.querySelector(".shorten-history");
-// console.log(shortenHistory);
 
-// shortenUrl.oninvalid = (e) => {
-//   e.target.setCustomValidity("");
-//   if (!e.target.validity.valid) {
-//     e.target.setCustomValidity("This field cannot be blank");
-//   }
-// };
-// shortenUrl.oninput = (e) => {
-//   e.target.setCustomValidity("");
-// };
-const copyDataFunction = async () => {
-  await render();
-  let observer = new MutationObserver((mutations) => {
-    for (let mutation of mutations) {
-      for (let node of mutation.addedNodes) {
-        if (node.matches('a[class="short-link"]')) {
-          try {
-            // const shortLink = document.querySelector(".short-link");
-            console.log(node);
-            // navigator.clipboard.writeText(shortLink.href);
-            console.log("text copied");
-            // if (dataCopied === "false") {
-            //   copyButton.setAttribute("data-copied", true);
-            // }
-          } catch (error) {
-            console.error(error.message);
-          }
-        }
-      }
-    }
+const handleDataError = () => {
+  shortenUrl.setCustomValidity("");
+  const valid = shortenUrl.checkValidity();
+  const dataError = document.querySelector("[data-error-message]");
+  if (!valid) {
+    dataError.setAttribute("data-error-message", true);
+    dataError.innerHTML = "Please add a link";
+    shortenUrl.setAttribute("data-valid", false);
+  }
+  shortenUrl.addEventListener("focus", () => {
+    dataError.setAttribute("data-error-message", false);
+    shortenUrl.setAttribute("data-valid", true);
   });
-  observer.observe(shortenHistoryContainer, {
-    subtree: true,
-    characterData: true,
-  });
-  copyDataFunction();
-
-  // if (navigator.clipboard) {
-  //   const copyButton = document.querySelector("copy-button");
-  //   const dataCopied = copyButton.getAttribute("data-copied");
-  //   console.log(dataCopied);
-  //   console.log(shortLink);
-
-  //   copyButton.addEventListener("click",  () => {
-  //     try {
-  //       navigator.clipboard.writeText(shortLink.href);
-  //       console.log("text copied");
-  //       if (dataCopied === "false") {
-  //         copyButton.setAttribute("data-copied", true);
-  //       }
-  //     } catch (error) {
-  //       console.error(error.message);
-  //     }
-  //   });
-  // }
 };
+
+// const copyDataFunction = async (elm) => {
+// await render();
+// let mList = document.getElementById('myList'),
+// try {
+//   options = {
+//     childList: true
+//   },
+//   observer = new MutationObserver(mCallback);
+
+//   function mCallback(mutations) {
+//     for (let mutation of mutations) {
+//       if (mutation.type === 'childList') {
+//         console.log('Mutation Detected: A child node has been added or removed.');
+//       }
+//     }
+//   }
+
+//   observer.observe(elm, options);
+// } catch (error) {
+//   console.error(error.message);
+// }
+
+//   const handleMutation= ()=>{
+//     const copyButton = document.querySelector(".copy-button");
+//     const shortLinkHref = document.querySelector('.short-link').getAttribute('href');
+//     console.log(copyButton,"\n",shortLinkHref);
+//     observer.disconnect();
+//   }
+//   let observer = new MutationObserver((mutations) => {
+//     for (let mutation of mutations) {
+//     if (mutation.type==='childList'&&mutation.addedNodes.length>0) {
+//       console.log("Yes")
+// mutation.addedNodes.forEach((node)=>{
+//   if (node.classList&&node.classList.contains('.shorten-history')) {
+//     handleMutation();
+//   }
+// })
+//     }
+//     }
+//   });
+//   observer.observe(shortenHistoryContainer, {
+//     subtree: true,
+//     childList: true,
+//   });
+// copyDataFunction();
+
+// if (navigator.clipboard) {
+//   const copyButton = document.querySelector("copy-button");
+//   const dataCopied = copyButton.getAttribute("data-copied");
+//   console.log(dataCopied);
+//   console.log(shortLink);
+
+//   copyButton.addEventListener("click",  () => {
+//     try {
+//       navigator.clipboard.writeText(shortLink.href);
+//       console.log("text copied");
+//       if (dataCopied === "false") {
+//         copyButton.setAttribute("data-copied", true);
+//       }
+//     } catch (error) {
+//       console.error(error.message);
+//     }
+//   });
+// }
+// };
 
 const navBar = document.querySelector(".nav-bar");
 const hamburgerMenu = document.querySelector(".hamber-menu");
